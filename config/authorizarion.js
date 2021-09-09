50cb3ef34be3e7d67fb3d778283dce76da3e97c4
korafdavid <okoroafordavid61@gmail.com>
2021-09-09 23:54:14 +0100

    updated status Codes

--- config/authorizarion.js
+++ config/authorizarion.js
@@ -4,7 +4,7 @@ const config = process.env;
 const verifyToken = (req, res, next) => {
     const token = req.body.token || req.query.token || req.headers["x-access-token"];
     if (!token) {
-        return res.status(403).send('UnAuthorized access');
+        return res.status(401).send('UnAuthorized access');
     }
     try {
         const decoded = jwt.verify(token, config.TOKEN_KEY);
